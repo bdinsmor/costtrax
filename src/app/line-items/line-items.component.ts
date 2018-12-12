05b96a5f68fc4e84e606ce9f13fe9cd597ad96e0
Brian Dinsmore <bdins@Brians-iMac.fios-router.home>
2018-12-12 01:00:27 -0500

    if /H could see me now

--- src/app/line-items/line-items.component.ts
+++ src/app/line-items/line-items.component.ts
@@ -177,7 +177,10 @@ export class LineItemsComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.bsConfig = Object.assign({}, { containerClass: this.colorTheme,  dateInputFormat: 'YYYY-MM-DD' });
+    this.bsConfig = Object.assign(
+      {},
+      { containerClass: this.colorTheme, dateInputFormat: 'YYYY-MM-DD' }
+    );
     this.subscription = this.authenticationService
       .getCreds()
       .subscribe(message => {
@@ -952,12 +955,12 @@ export class LineItemsComponent implements OnInit, OnDestroy {
     if (!item.details.startDate || !item.details.endDate) {
       return;
     }
-    console.log("start date: " + item.details.startDate);
+
     const diff = Math.abs(
       new Date(item.details.startDate).getTime() -
         new Date(item.details.endDate).getTime()
     );
-console.log("diff: " + diff);
+
     let diffDays = Math.ceil(diff / (1000 * 3600 * 24));
 
     if (diffDays === 0) {
