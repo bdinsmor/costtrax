36d4dc4627fcf0dfc9ae9a5e6cc124f4cb39980d
David C <davidchouse@users.noreply.github.com>
2018-12-16 10:27:44 -0500

    devops

---
+++ devops/update.prod.sh
@@ -0,0 +1,3 @@
+set -e
+AWS_PROFILE="informa"
+aws --profile $AWS_PROFILE --region us-east-1 cloudformation update-stack --stack-name prod-costtrax-ui --parameters "file://prod.params.json" --template-url "https://s3.amazonaws.com/devops-610665873581-us-east-1/infrastructure/website.yaml" --capabilities CAPABILITY_NAMED_IAM --notification-arns arn:aws:sns:us-east-1:610665873581:informa-devops-cloudformation-us-east-1
