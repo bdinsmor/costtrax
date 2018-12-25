4fa24744ace94553fadcc050627cdec0fefad239
Brian Dinsmore <bdinsmor@gmail.com>
2018-12-25 12:03:55 -0500

    changes

--- src/environments/.env.ts
+++ src/environments/.env.ts
@@ -1,7 +1,3 @@
 export default {
-<<<<<<< HEAD
-  npm_package_version: '1.0.3'
-=======
-  npm_package_version: '1.1.1'
->>>>>>> e19cc6299260b4c3bfc6954529e87889607740ea
+  npm_package_version: '1.1.3'
 };
