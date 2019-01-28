5b4da84a1cadfa6b184637792b5dfc33d2b9ea2c
Brian Dinsmore <bdins@Brians-iMac.fios-router.home>
2019-01-27 20:05:46 -0500

    password reset

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CountdownModule } from 'ngx-countdown';
 
 import { MaterialModule } from '../material.module';
 import { SharedModule } from '../shared';
@@ -15,6 +16,7 @@ import { SyncDialogComponent } from './sync.dialog';
     ReactiveFormsModule,
     SharedModule,
     MaterialModule,
+    CountdownModule,
     LoginRoutingModule
   ],
   declarations: [LoginComponent, SyncDialogComponent, ResetComponent],
